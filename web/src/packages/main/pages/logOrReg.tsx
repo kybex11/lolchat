11ccf151c88b6fe21782ecc96015075b68c55dae
kybex11 <artm.22@internet.ru>
2024-11-02 18:35:53 +0300

    fix /router route

--- web/src/packages/main/pages/logOrReg.tsx
+++ web/src/packages/main/pages/logOrReg.tsx
@@ -1,48 +1,20 @@
 import { Link } from "react-router-dom"
 import Navbar from "../components/navbar"
-import { useEffect, useState } from 'react';
-import MobileNavbar from "../components/mobilenavbar";
+import { checkLogin } from "../cookies";
 
 export default function LogOrReg() {
-    const [isMobile, setIsMobile] = useState<boolean>(false);
-
-    useEffect(() => {
-        const userAgent = navigator.userAgent.toLowerCase();
-        const isMobileDevice = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/.test(userAgent);
-
-        setIsMobile(isMobileDevice);
-    }, []);
-
-    return (
-        <>
-            {isMobile ? <MobileComponent /> : <DesktopComponent />}
-        </>
-    )
+    if (checkLogin()) {
+        window.location.href = "/web"; 
+     } else {
+        return (
+         <>
+         <Navbar/>
+         <div className="pcontainer">
+             <Link className="link" to="register">Register</Link>
+             <h1>or</h1>
+             <Link className="link" to="login">Login</Link>
+         </div>
+         </>
+     ) 
+     }
 }
-
- function DesktopComponent() {
-    return (
-        <>
-        <Navbar/>
-        <div className="pcontainer">
-            <Link className="link" to="register">Register</Link>
-            <h1>or</h1>
-            <Link className="link" to="login">Login</Link>
-        </div>
-        </>
-    )
-}
-
-function MobileComponent() {
-    return (
-        <>
-        <MobileNavbar/>
-        <div className="pcontainer">
-            <Link className="link_m" to="register">Register</Link>
-            <h1>or</h1>
-            <Link className="link_m" to="login">Login</Link>
-        </div>
-        </>
-    )
-}
-
